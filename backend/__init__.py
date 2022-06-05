b65acf10970a3065fc6f26e1ef11c226b9eee90e
HajaSC <hajasc@protonmail.com>
2022-06-05 14:51:00 -0400

    Adds pytest modules, secret key, config link

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,9 +4,11 @@ from config import config
 
 db = SQLAlchemy()
 
-def create_app(config):
+def create_app(config_rule):
     
     app = Flask(__name__)
+    app.config.from_object(config[config_rule])
+
     app.config.from_pyfile("../config.py")
 
     db.init_app(app)
--- config.py
+++ config.py
@@ -3,15 +3,20 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    @staticmethod
+    def init_app(app):
+        pass
+
 class DevConfig(Config):
     DEBUG = True
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "postgresql://databaseuser:P@ssw0rd@localhost:5432/fake_admin"
 
 class TestConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL")
 
-config = {
+config = {    
     'development': DevConfig,
     'testing': TestConfig,
     'default': DevConfig
