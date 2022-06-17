d73e29fa2e948dfe5b81693d1f13f5c7eb21a924
HajaSC <hajasc@protonmail.com>
2022-06-17 00:53:01 -0400

    Adjusts models

--- backend/models.py
+++ backend/models.py
@@ -14,17 +14,17 @@ class Artists(db.Model):
     __tablename__ = "artists"
     id = db.Column(db.Integer, primary_key=True)  
     name = db.Column(db.String,unique=True, nullable=False)
-    songs = db.relationship('Songs', backref='artist', lazy=True) 
+    songs = db.relationship('Songs', backref='name', lazy=True) 
 
     def __repr__(self):
-        return f"International Artist {self.artist}"
+        return f"International Artist {self.name}"
 
 class Songs(db.Model):
     __tablename__ = "hits"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     genre = db.Column(db.String, nullable=True)  
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)      
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=True)      
 
     def __repr__(self):
         return f"{self.title}"
